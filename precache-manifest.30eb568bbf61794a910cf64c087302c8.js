1fd827995fe69eb9b2aa96ab533eae3dd4dd7b96
ibrahim arbi <ibrahim.arbi@frantic.com>
2019-10-03 19:22:33 +0300

    Updates

---
+++ precache-manifest.30eb568bbf61794a910cf64c087302c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b0ea541699ebe35ba747ab86ed13624",
+    "url": "/StatusCheckerTask/index.html"
+  },
+  {
+    "revision": "a3411b484f3634fa848c",
+    "url": "/StatusCheckerTask/static/css/main.c2873c60.chunk.css"
+  },
+  {
+    "revision": "b46f4b0154e90eb01f87",
+    "url": "/StatusCheckerTask/static/js/2.5a2b398a.chunk.js"
+  },
+  {
+    "revision": "a3411b484f3634fa848c",
+    "url": "/StatusCheckerTask/static/js/main.67fd3a1e.chunk.js"
+  },
+  {
+    "revision": "05e133143dbc0d786293",
+    "url": "/StatusCheckerTask/static/js/runtime-main.509d228f.js"
+  }
+]);
